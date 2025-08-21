0f1ff154ebb7d3b5d11e2f75d2ca450ccf956419
CoolioTT <thewordishi@gmail.com>
2025-08-21 21:34:39 +0000

    Remove unused folder and cleanup files

--- client/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-function App() {
-  return <h1>Welcome to Valorant Scrim Platform</h1>
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
